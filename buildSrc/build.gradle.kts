b8bc1f8458f0403de7b4d9efb0528bb7eb0c0973
KyleBorth-Gravie <kyle.borth@gravie.com>
2023-08-01 14:18:03 -0500

    Updated dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.4.2")
+    implementation("com.android.tools.build:gradle:8.0.2")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.45")
     implementation(kotlin(module = "gradle-plugin", version = "1.8.10"))
 }
